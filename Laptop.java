002a51d28fa0144d1b8344a448147509eaa70334
Fennellis <westgeze@gmail.com>
2023-12-09 20:25:51 +0300

    v.8.0.0.8.5 (Добавлено главное меню)

--- Laptop.java
+++ Laptop.java
@@ -1,17 +1,70 @@
 public class Laptop {
-    String model;
-    int ram;
-    int hdCap;
-    String os;
-    String color;
+    private String manufacturer;
+    private int ram;
+    private int hdCap;
+    private String os;
+    private String color;
+
+    public void setManufacturer(String manufacturer){
+        this.manufacturer = manufacturer;
+    }
+
+    public void setRAM(int ram){
+        this.ram = ram;
+    }
+
+    public void setHDCap(int hdCap){
+        this.hdCap = hdCap;
+    }
+
+    public void setOS(String os){
+        this.os = os;
+    }
+
+    public void setColor(String color){
+        this.color = color;
+    }
+
+    public String getManufacturer(){
+        return manufacturer;
+    }
+
+    public int getRAM(){
+        return ram;
+    }
+
+    public int getHDCap(){
+        return hdCap;
+    }
+
+    public String getOS(){
+        return os;
+    }
+
+    public String getColor(){
+        return color;
+    }
+
 
     @Override
     public String toString() {
-        return String.format("%s: %d %d %s %s", model, ram, hdCap, os, color);
+        return String.format("Manuf: %10s   RAM: %2d GB   HDCap: %5d GB   OS: %10s   Color: %10s", manufacturer, ram, hdCap, os, color);
     }
 
     @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj instanceof Laptop){
+            Laptop laptop = (Laptop) obj;
+            return manufacturer.equals(laptop.manufacturer) && ram == laptop.ram && hdCap == laptop.hdCap &&
+            os.equals(laptop.os) && color.equals(laptop.color);
+        }
+
+        return false;
+    }
+    @Override
     public int hashCode() {
-        return 3*model.hashCode() + 5*ram + 7*hdCap + 11*os.hashCode() + 13*color.hashCode();
+        return manufacturer.hashCode() + 5*ram + 7*hdCap + 11*os.hashCode() + 13*color.hashCode();
     }
 }
--- Program.java
+++ Program.java
@@ -21,50 +21,83 @@ import java.util.Scanner;
 public class Program {
 
     public static void main(String[] args) {
-        HashSet<Laptop> set = CreateRandomSet(100);
-        HashSet<Laptop> select = Selection(set);
+        HashSet<Laptop> catalog = CreateRandomSet(100);
+        HashSet<Laptop> select;
+        Laptop filter = new Laptop();
 
-        System.out.println("Все ноутбуки");
-        for (Laptop laptop : set) {
-            System.out.println(laptop);
-        }
-        System.out.println("Выборка");
-        for (Laptop laptop : select) {
-            System.out.println(laptop);
+        Scanner sc = new Scanner(System.in);
+        String option;
+        LinkedList<String> options = new LinkedList<>(Arrays.asList("1", "2", "3", "4", "5"));
+        boolean escape = false;
+        while (!escape) {
+            MainMenu();
+            option = sc.nextLine();
+            System.out.println();
+            if (options.contains(option)) {
+                switch (option) {
+                    case "1":  // Весь каталог
+                        for (Laptop laptop : catalog) {
+                            System.out.println(laptop);
+                        }
+                        System.out.printf("\nВсего позиций: %d\n\n\n", catalog.size());
+                        break;
+                    case "2":  // Каталог с фильтрами (ИСПРАВИТЬ БАГИ)
+                        select = Selection(catalog, filter);
+                        for (Laptop laptop : select) {
+                            System.out.println(laptop);
+                        }
+                        break;
+                    case "3":  // Показать фильтры
+                        System.out.println("Текущие фильтры:\n" + filter.toString() + "\n");
+                        break;
+                    case "4":  // Настройка фильтров
+                        filter = GetStandart();
+                        break;
+                    case "5":  // Выход
+                        escape = true;
+                        break;
+                }
+            }
+            else{
+                System.out.println("Некорректный ввод команды. Повторите попытку.");
+            }
         }
     }
 
     private static HashSet<Laptop> CreateRandomSet(int n){
+        ArrayList<String> manufactur = new ArrayList<>(Arrays.asList("ASUS", "HP", "Lenovo", "MSI", "Apple"));
         ArrayList<Integer> ram = new ArrayList<>(Arrays.asList(8, 16, 32));
         ArrayList<Integer> hdCap = new ArrayList<>(Arrays.asList(256, 512, 1024, 2048));
-        ArrayList<String> os = new ArrayList<>(Arrays.asList("Linux", "Windows", "IOS"));
+        ArrayList<String> os = new ArrayList<>(Arrays.asList("Linux", "Windows"));
         ArrayList<String> color = new ArrayList<>(Arrays.asList("Black", "White", "Gold", "Pink", "Gray"));
 
         Random rnd = new Random();
         HashSet<Laptop> laptops = new HashSet<>();
         for (int i = 0; i < n; i++) {
             Laptop current = new Laptop();
-            current.model = Integer.toString(i);
-            current.ram = ram.get(rnd.nextInt(ram.size()));
-            current.hdCap = hdCap.get(rnd.nextInt(hdCap.size()));
-            current.os = os.get(rnd.nextInt(os.size()));
-            current.color = color.get(rnd.nextInt(color.size()));
+            current.setManufacturer(manufactur.get(rnd.nextInt(manufactur.size())));
+            current.setRAM(ram.get(rnd.nextInt(ram.size())));
+            current.setHDCap(hdCap.get(rnd.nextInt(hdCap.size())));
+            if(current.getManufacturer().equals("Apple"))
+                current.setOS("IOS");
+            else
+                current.setOS(os.get(rnd.nextInt(os.size())));
+            current.setColor(color.get(rnd.nextInt(color.size())));
             laptops.add(current);
         }
 
         return laptops;
     }
 
-    private static HashSet<Laptop> Selection(HashSet<Laptop> set){
-        Laptop standart = GetStandart();
+    private static HashSet<Laptop> Selection(HashSet<Laptop> set, Laptop standart){
         System.out.println(standart.toString());
         HashSet<Laptop> select = new HashSet<>();
         for (Laptop laptop : set) {
-            if (laptop.ram >= standart.ram && laptop.hdCap >= standart.hdCap && 
-                (laptop.color.equals(standart.color) || standart.color == null) &&
-                (laptop.os.equals(standart.os) || standart.os == null)){
-                select.add(laptop);
-            }
+            if (laptop.getRAM() >= standart.getRAM() && laptop.getHDCap() >= standart.getHDCap() && 
+                (laptop.getColor().equals(standart.getColor()) || standart.getColor() == null) &&
+                (laptop.getOS().equals(standart.getOS()) || standart.getOS() == null) && 
+                (laptop.getManufacturer().equals(standart.getManufacturer()) || standart.getManufacturer() == null))
+                    select.add(laptop);
         }
 
         return select;
@@ -74,46 +107,60 @@ public class Program {
         Scanner sc = new Scanner(System.in);
         String option = "";
         String value;
-        LinkedList<String> options = new LinkedList<>(Arrays.asList("1", "2", "3", "4", "5"));
+        LinkedList<String> options = new LinkedList<>(Arrays.asList("1", "2", "3", "4", "5", "6"));
         Laptop standart = new Laptop();
         while (true) {
-            Menu();
+            SelectionMenu();
             option = sc.nextLine();
             if (!options.contains(option)){
                 System.out.println("Некорректный ввод");
                 continue;
             }
-            if (option.equals("5"))
+            if (option.equals("6"))
                 break;
             System.out.println("Введите значение: ");
             value = sc.nextLine();
 
             switch (option) {
                 case "1":
-                    standart.ram = Integer.parseInt(value);
+                    standart.setManufacturer(value);
                     break;
                 case "2":
-                    standart.hdCap= Integer.parseInt(value);
+                    standart.setRAM(Integer.parseInt(value));
                     break;
                 case "3":
-                    standart.os = value;
+                    standart.setHDCap(Integer.parseInt(value));
                     break;
                 case "4":
-                    standart.color = value;
+                    standart.setOS(value);
+                    break;
+                case "5":
+                    standart.setColor(value);
                     break;
             }
         }
-
+        
         return standart;
     }
 
-    private static void Menu(){
+    private static void SelectionMenu(){
         System.out.println("Выберите параметр:");
-        System.out.println("1 - ОЗУ");
-        System.out.println("2 - Объем ЖД");
-        System.out.println("3 - Операционная система");
-        System.out.println("4 - Цвет");
-        System.out.println("5 - Применить фильтры");
+        System.out.println("1 - Производитель");
+        System.out.println("2 - ОЗУ");
+        System.out.println("3 - Объем ЖД");
+        System.out.println("4 - Операционная система");
+        System.out.println("5 - Цвет");
+        System.out.println("6 - Применить фильтры");
         System.out.print("-> ");
+    }
+
+    private static void MainMenu(){
+        System.out.println("Главное меню.");
+        System.out.println("1. Показать весь каталог.");
+        System.out.println("2. Показать каталог с фильрами.");
+        System.out.println("3. Показать текущие фильтры.");
+        System.out.println("4. Настройка фильтров.");
+        System.out.println("5. Выход\n");
+        System.out.print("Введите номер опции -> ");
     }
 }
